4b3e92c84e1ac932656605a1dd581c607c6547f2
fire8327 <fire83274@gmail.com>
2025-04-16 10:47:19 +0300

    upd check of profileCompleted

--- pages/resumes/index.vue
+++ pages/resumes/index.vue
@@ -1,46 +1,54 @@
 <template>
-    <div class="flex flex-col gap-6">
-        <p class="mainHeading">Фильтрация</p>
-        <div class="flex flex-col items-center gap-6 w-full text-[#1C1C1C]">
-            <FormKit v-model="searchQuery.name" type="text"
-                placeholder="Желаемая должность" name="Желаемая должность" outer-class="w-full md:w-2/3 lg:w-1/2"
-                input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-sky-500 shadow-md" />
-            <div class="flex items-center lg:items-start gap-4 max-lg:flex-col w-full md:w-2/3 lg:w-1/2">
-                <FormKit v-model="searchQuery.exp" type="select" :options="['Без опыта', 'До 1 года', '1-3 года', '3-5 лет', 'Более 5 лет']" placeholder="Опыт работы" name="Опыт работы" outer-class="w-full lg:w-1/2" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-sky-500 shadow-md"/>
-                <FormKit v-model="searchQuery.education" type="select" :options="['Среднее', 'Среднее специальное', 'Неоконченное высшее', 'Высшее', 'Бакалавр', 'Магистр', 'Аспирантура']" placeholder="Образование" name="Образование" outer-class="w-full lg:w-1/2" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-sky-500 shadow-md"/>
+    <template v-if="userStore.profileCompleted">
+        <div class="flex flex-col gap-6">
+            <p class="mainHeading">Фильтрация</p>
+            <div class="flex flex-col items-center gap-6 w-full text-[#1C1C1C]">
+                <FormKit v-model="searchQuery.name" type="text"
+                    placeholder="Желаемая должность" name="Желаемая должность" outer-class="w-full md:w-2/3 lg:w-1/2"
+                    input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-sky-500 shadow-md" />
+                <div class="flex items-center lg:items-start gap-4 max-lg:flex-col w-full md:w-2/3 lg:w-1/2">
+                    <FormKit v-model="searchQuery.exp" type="select" :options="['Без опыта', 'До 1 года', '1-3 года', '3-5 лет', 'Более 5 лет']" placeholder="Опыт работы" name="Опыт работы" outer-class="w-full lg:w-1/2" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-sky-500 shadow-md"/>
+                    <FormKit v-model="searchQuery.education" type="select" :options="['Среднее', 'Среднее специальное', 'Неоконченное высшее', 'Высшее', 'Бакалавр', 'Магистр', 'Аспирантура']" placeholder="Образование" name="Образование" outer-class="w-full lg:w-1/2" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-sky-500 shadow-md"/>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="flex flex-col gap-6" v-if="resumes && resumes.length > 0">
-        <p class="mainHeading">Список резюме</p>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 text-[#1C1C1C]">
-            <div class="flex flex-col gap-4 p-4 rounded-xl shadow-lg bg-white" v-for="resume in resumes">
-                <NuxtLink :to="`/resumes/resume-${resume.id}`" class="cursor-pointer self-end transition-all duration-500 hover:scale-110">
-                    <Icon class="text-3xl text-sky-500" name="material-symbols:eye-tracking-rounded" />
-                </NuxtLink>
-                <p>{{ resume.name }}</p>
-                <p><span class="font-semibold font-mono">Опыт: </span>{{ resume.exp }}</p>
-                <p><span class="font-semibold font-mono">Образование: </span>{{ resume.education }}</p>
-                <p><span class="font-semibold font-mono">Зарплата: </span>{{ resume.salary.toLocaleString() }} ₽</p>
-                <button @click="sendOffer(resume.id)"
-                    :class="[isOffered(resume.id) ? 'opacity-50' : 'cursor-pointer hover:bg-transparent hover:text-sky-500']"
-                    :disabled="isOffered(resume.id)"
-                    class="w-full py-1.5 px-4 rounded-xl border border-sky-500 bg-sky-500 text-white transition-all duration-500">{{
-                        isOffered(resume.id) ? 'Предложение отправлено' : 'Предложить работу' }}</button>
+        <div class="flex flex-col gap-6" v-if="resumes && resumes.length > 0">
+            <p class="mainHeading">Список резюме</p>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 text-[#1C1C1C]">
+                <div class="flex flex-col gap-4 p-4 rounded-xl shadow-lg bg-white" v-for="resume in resumes">
+                    <NuxtLink :to="`/resumes/resume-${resume.id}`" class="cursor-pointer self-end transition-all duration-500 hover:scale-110">
+                        <Icon class="text-3xl text-sky-500" name="material-symbols:eye-tracking-rounded" />
+                    </NuxtLink>
+                    <p>{{ resume.name }}</p>
+                    <p><span class="font-semibold font-mono">Опыт: </span>{{ resume.exp }}</p>
+                    <p><span class="font-semibold font-mono">Образование: </span>{{ resume.education }}</p>
+                    <p><span class="font-semibold font-mono">Зарплата: </span>{{ resume.salary.toLocaleString() }} ₽</p>
+                    <button @click="sendOffer(resume.id)"
+                        :class="[isOffered(resume.id) ? 'opacity-50' : 'cursor-pointer hover:bg-transparent hover:text-sky-500']"
+                        :disabled="isOffered(resume.id)"
+                        class="w-full py-1.5 px-4 rounded-xl border border-sky-500 bg-sky-500 text-white transition-all duration-500">{{
+                            isOffered(resume.id) ? 'Предложение отправлено' : 'Предложить работу' }}</button>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="flex flex-col gap-6 items-center text-center" v-else>
-        <p class="mainHeading">Не найдено ни одного резюме</p>
-        <div class="flex items-center gap-4 max-md:flex-col w-full md:w-2/3">
-            <button @click="cancelFilters"
-                class="cursor-pointer w-full md:w-1/2 py-1.5 px-4 rounded-xl text-white bg-sky-500 border border-sky-500 transition-all duration-500 hover:bg-transparent hover:text-sky-500">Сбросить
-                фильтры</button>
-            <NuxtLink to="/"
-                class="w-full md:w-1/2 py-1.5 px-4 rounded-xl hover:text-white hover:bg-sky-500 border border-sky-500 transition-all duration-500 bg-transparent text-sky-500">
-                Вернуться на главную</NuxtLink>
+        <div class="flex flex-col gap-6 items-center text-center" v-else>
+            <p class="mainHeading">Не найдено ни одного резюме</p>
+            <div class="flex items-center gap-4 max-md:flex-col w-full md:w-2/3">
+                <button @click="cancelFilters"
+                    class="cursor-pointer w-full md:w-1/2 py-1.5 px-4 rounded-xl text-white bg-sky-500 border border-sky-500 transition-all duration-500 hover:bg-transparent hover:text-sky-500">Сбросить
+                    фильтры</button>
+                <NuxtLink to="/"
+                    class="w-full md:w-1/2 py-1.5 px-4 rounded-xl hover:text-white hover:bg-sky-500 border border-sky-500 transition-all duration-500 bg-transparent text-sky-500">
+                    Вернуться на главную</NuxtLink>
+            </div>
         </div>
-    </div>
+    </template>
+    <template v-else>
+        <div class="flex flex-col gap-6 items-center justify-center text-center">
+            <p class="mainHeading max-w-xl">Продолжите заполнения профиля или зарегистрируйтесь, чтобы пользоваться всеми функциями сайта</p>
+            <NuxtLink to="/profile" class="mx-auto px-4 py-2 border border-sky-500 text-sky-500 rounded-full w-[160px] text-center transition-all duration-500 hover:text-white hover:bg-sky-500">Продолжить</NuxtLink>
+        </div>
+    </template>
 </template>
 
 <script setup>
--- pages/vacancies/index.vue
+++ pages/vacancies/index.vue
@@ -1,49 +1,57 @@
 <template>
-    <div class="flex flex-col gap-6">
-        <p class="mainHeading">Фильтрация</p>
-        <div class="flex flex-col items-center gap-6 w-full text-[#1C1C1C]">
-            <FormKit v-model="searchQuery.name" type="text"
-                placeholder="Наименование вакансии" name="Наименование вакансии" outer-class="w-full md:w-2/3 lg:w-1/2"
-                input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-sky-500 shadow-md" />
-            <div class="flex items-center lg:items-start gap-4 max-lg:flex-col w-full md:w-2/3 lg:w-1/2">
-                <FormKit v-model="searchQuery.exp" type="select" :options="['Без опыта', 'До 1 года', '1-3 года', '3-5 лет', 'Более 5 лет']" placeholder="Опыт работы" name="Опыт работы" outer-class="w-full lg:w-1/3" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-sky-500 shadow-md"/>
-                <FormKit v-model="searchQuery.schedule" type="select" :options="['5/2', '6/1', '2/2', '1/2', '1/3', 'Неделя через неделю', 'Дежурства', 'Гибкий график']" placeholder="График" name="График" outer-class="w-full lg:w-1/3" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-sky-500 shadow-md"/>
-                <FormKit v-model="searchQuery.format" type="select" :options="['Удалённый', 'Офис', 'Гибрид', 'Вахта', 'Проектный']" placeholder="Формат" name="Формат" outer-class="w-full lg:w-1/3" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-sky-500 shadow-md"/>
+    <template v-if="userStore.profileCompleted">
+        <div class="flex flex-col gap-6">
+            <p class="mainHeading">Фильтрация</p>
+            <div class="flex flex-col items-center gap-6 w-full text-[#1C1C1C]">
+                <FormKit v-model="searchQuery.name" type="text"
+                    placeholder="Наименование вакансии" name="Наименование вакансии" outer-class="w-full md:w-2/3 lg:w-1/2"
+                    input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-sky-500 shadow-md" />
+                <div class="flex items-center lg:items-start gap-4 max-lg:flex-col w-full md:w-2/3 lg:w-1/2">
+                    <FormKit v-model="searchQuery.exp" type="select" :options="['Без опыта', 'До 1 года', '1-3 года', '3-5 лет', 'Более 5 лет']" placeholder="Опыт работы" name="Опыт работы" outer-class="w-full lg:w-1/3" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-sky-500 shadow-md"/>
+                    <FormKit v-model="searchQuery.schedule" type="select" :options="['5/2', '6/1', '2/2', '1/2', '1/3', 'Неделя через неделю', 'Дежурства', 'Гибкий график']" placeholder="График" name="График" outer-class="w-full lg:w-1/3" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-sky-500 shadow-md"/>
+                    <FormKit v-model="searchQuery.format" type="select" :options="['Удалённый', 'Офис', 'Гибрид', 'Вахта', 'Проектный']" placeholder="Формат" name="Формат" outer-class="w-full lg:w-1/3" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-sky-500 shadow-md"/>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="flex flex-col gap-6" v-if="vacancies && vacancies.length > 0">
-        <p class="mainHeading">Список вакансий</p>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 text-[#1C1C1C]">
-            <div class="flex flex-col gap-4 p-4 rounded-xl shadow-lg bg-white" v-for="vacancy in vacancies">
-                <NuxtLink :to="`/vacancies/vacancy-${vacancy.id}`" class="cursor-pointer self-end transition-all duration-500 hover:scale-110">
-                    <Icon class="text-3xl text-sky-500" name="material-symbols:eye-tracking-rounded" />
-                </NuxtLink>
-                <p>{{ vacancy.name }}</p>
-                <p class="line-clamp-2">{{ vacancy.desc }}</p>
-                <p><span class="font-semibold font-mono">Опыт: </span>{{ vacancy.exp }}</p>
-                <p><span class="font-semibold font-mono">Формат: </span>{{ vacancy.format }}</p>
-                <p><span class="font-semibold font-mono">График: </span>{{ vacancy.schedule }}</p>
-                <p><span class="font-semibold font-mono">Зарплата: </span>{{ vacancy.salary.toLocaleString() }} ₽</p>
-                <button @click="sendResponse(vacancy.id)"
-                    :class="[isResponded(vacancy.id) ? 'opacity-50' : 'cursor-pointer hover:bg-transparent hover:text-sky-500']"
-                    :disabled="isResponded(vacancy.id)"
-                    class="w-full py-1.5 px-4 rounded-xl border border-sky-500 bg-sky-500 text-white transition-all duration-500">{{
-                        isResponded(vacancy.id) ? 'Отклик отправлен' : 'Откликнуться' }}</button>
+        <div class="flex flex-col gap-6" v-if="vacancies && vacancies.length > 0">
+            <p class="mainHeading">Список вакансий</p>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 text-[#1C1C1C]">
+                <div class="flex flex-col gap-4 p-4 rounded-xl shadow-lg bg-white" v-for="vacancy in vacancies">
+                    <NuxtLink :to="`/vacancies/vacancy-${vacancy.id}`" class="cursor-pointer self-end transition-all duration-500 hover:scale-110">
+                        <Icon class="text-3xl text-sky-500" name="material-symbols:eye-tracking-rounded" />
+                    </NuxtLink>
+                    <p>{{ vacancy.name }}</p>
+                    <p class="line-clamp-2">{{ vacancy.desc }}</p>
+                    <p><span class="font-semibold font-mono">Опыт: </span>{{ vacancy.exp }}</p>
+                    <p><span class="font-semibold font-mono">Формат: </span>{{ vacancy.format }}</p>
+                    <p><span class="font-semibold font-mono">График: </span>{{ vacancy.schedule }}</p>
+                    <p><span class="font-semibold font-mono">Зарплата: </span>{{ vacancy.salary.toLocaleString() }} ₽</p>
+                    <button @click="sendResponse(vacancy.id)"
+                        :class="[isResponded(vacancy.id) ? 'opacity-50' : 'cursor-pointer hover:bg-transparent hover:text-sky-500']"
+                        :disabled="isResponded(vacancy.id)"
+                        class="w-full py-1.5 px-4 rounded-xl border border-sky-500 bg-sky-500 text-white transition-all duration-500">{{
+                            isResponded(vacancy.id) ? 'Отклик отправлен' : 'Откликнуться' }}</button>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="flex flex-col gap-6 items-center text-center" v-else>
-        <p class="mainHeading">Не найдено ни одной вакансии</p>
-        <div class="flex items-center gap-4 max-md:flex-col w-full md:w-2/3">
-            <button @click="cancelFilters"
-                class="cursor-pointer w-full md:w-1/2 py-1.5 px-4 rounded-xl text-white bg-sky-500 border border-sky-500 transition-all duration-500 hover:bg-transparent hover:text-sky-500">Сбросить
-                фильтры</button>
-            <NuxtLink to="/"
-                class="w-full md:w-1/2 py-1.5 px-4 rounded-xl hover:text-white hover:bg-sky-500 border border-sky-500 transition-all duration-500 bg-transparent text-sky-500">
-                Вернуться на главную</NuxtLink>
+        <div class="flex flex-col gap-6 items-center text-center" v-else>
+            <p class="mainHeading">Не найдено ни одной вакансии</p>
+            <div class="flex items-center gap-4 max-md:flex-col w-full md:w-2/3">
+                <button @click="cancelFilters"
+                    class="cursor-pointer w-full md:w-1/2 py-1.5 px-4 rounded-xl text-white bg-sky-500 border border-sky-500 transition-all duration-500 hover:bg-transparent hover:text-sky-500">Сбросить
+                    фильтры</button>
+                <NuxtLink to="/"
+                    class="w-full md:w-1/2 py-1.5 px-4 rounded-xl hover:text-white hover:bg-sky-500 border border-sky-500 transition-all duration-500 bg-transparent text-sky-500">
+                    Вернуться на главную</NuxtLink>
+            </div>
         </div>
-    </div>
+    </template>
+    <template v-else>
+        <div class="flex flex-col gap-6 items-center justify-center text-center">
+            <p class="mainHeading max-w-xl">Продолжите заполнения профиля или зарегистрируйтесь, чтобы пользоваться всеми функциями сайта</p>
+            <NuxtLink to="/profile" class="mx-auto px-4 py-2 border border-sky-500 text-sky-500 rounded-full w-[160px] text-center transition-all duration-500 hover:text-white hover:bg-sky-500">Продолжить</NuxtLink>
+        </div>
+    </template>
 </template>
 
 <script setup>
