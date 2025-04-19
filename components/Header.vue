2aa97724e89347d3b5479791ef0f6699ec8c1202
fire8327 <fire83274@gmail.com>
2025-04-19 04:03:02 +0300

    upd header

--- components/Header.vue
+++ components/Header.vue
@@ -1,27 +1,79 @@
 <template>
     <header class="grid-container py-4 text-lg font-medium relative">
-        <div class="w-full flex items-center gap-6 justify-between rounded-xl shadow-[0px_0px_20px_-13px_black] px-4 py-2 bg-[#2C2C2C]">
-            <NuxtLink to="/" class="flex items-center gap-2 text-sky-500 transition-all duration-500 hover:opacity-70">
-                <Icon class="text-3xl" name="hugeicons:job-search"/>
+        <!-- Десктопная версия -->
+        <div
+            class="w-full flex items-center gap-4 justify-between px-4 py-2 lg:bg-[#2C2C2C]/90 lg:rounded-xl lg:shadow-[0px_0px_20px_-13px_black]">
+            <NuxtLink to="/"
+                class="flex items-center gap-2 text-sky-500 transition-all duration-500 hover:opacity-70">
+                <Icon class="text-3xl" name="hugeicons:job-search" />
                 <span class="text-xl font-mono font-semibold">QuickServe</span>
             </NuxtLink>
-            <div class="flex items-center gap-6 max-lg:absolute max-lg:w-full max-lg:flex-col max-lg:py-6 text-white max-lg:bg-[#2C2C2C] max-lg:left-0 max-lg:border-t border-sky-500 transition-all duration-500 z-[5]" :class="isMenuShow ? 'max-lg:top-full' : 'max-lg:top-0 max-lg:-translate-y-full'">
-                <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full" to="/">Главная</NuxtLink>
-                <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full" to="/vacancies" v-if="userStore.role === 'applicant'">Вакансии</NuxtLink>
-                <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full" to="/resumes" v-if="userStore.role === 'employer'">Резюме</NuxtLink>
-                <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full" to="/about">О нас</NuxtLink>
-                <NuxtLink to="/auth" class="flex transition-all duration-500 hover:opacity-70">
-                    <Icon class="text-3xl text-sky-500" name="material-symbols:person"/>
+            <div class="flex items-center gap-4 lg:gap-6 text-white max-lg:hidden">
+                <NuxtLink to="/"
+                    class="px-3 py-1 bg-[#3C3C3C] rounded-xl shadow-md transition-all duration-500 hover:bg-[#000000]/15 hover:border-sky-500 border border-transparent">
+                    Главная
+                </NuxtLink>
+                <NuxtLink to="/vacancies" v-if="userStore.role === 'applicant'"
+                    class="px-3 py-1 bg-[#3C3C3C] rounded-xl shadow-md transition-all duration-500 hover:bg-[#000000]/15 hover:border-sky-500 border border-transparent">
+                    Вакансии
+                </NuxtLink>
+                <NuxtLink to="/resumes" v-if="userStore.role === 'employer'"
+                    class="px-3 py-1 bg-[#3C3C3C] rounded-xl shadow-md transition-all duration-500 hover:bg-[#000000]/15 hover:border-sky-500 border border-transparent">
+                    Резюме
+                </NuxtLink>
+                <NuxtLink to="/about"
+                    class="px-3 py-1 bg-[#3C3C3C] rounded-xl shadow-md transition-all duration-500 hover:bg-[#000000]/15 hover:border-sky-500 border border-transparent">
+                    О нас
                 </NuxtLink>
             </div>
-            <button @click="isMenuShow = !isMenuShow" class="lg:hidden flex cursor-pointer">
-                <Icon class="text-3xl text-sky-500" name="ic:round-menu"/>
-            </button>
+            <div class="flex items-center gap-4">
+                <NuxtLink to="/auth" class="flex transition-all duration-500 hover:opacity-70 max-lg:hidden">
+                    <Icon class="text-3xl text-sky-500" name="material-symbols:person" />
+                </NuxtLink>
+                <button @click="isMenuShow = !isMenuShow" class="lg:hidden flex cursor-pointer">
+                    <Icon class="text-3xl text-sky-500"
+                        :name="isMenuShow ? 'material-symbols:close' : 'ic:round-menu'" />
+                </button>
+            </div>
         </div>
-        <div @click="isMenuShow = false" class="lg:hidden fixed z-[4] inset-0 bg-black/30 top-[85px]" :class="{'max-lg:hidden' : !isMenuShow}"></div>
-        <button type="button" @click="messageTitle = null" class="fixed top-10 right-10 z-[11] cursor-pointer flex items-center gap-2 px-6 py-2 text-lg rounded-xl w-fit font-medium font-mono border-white border bg-[#2C2C2C] shadow-[0px_0px_13px_-7px_black]" :class="messageType ? ' text-white' : 'text-red-500'" v-if="messageTitle">
-            <Icon class="text-3xl" name="material-symbols:close-small-rounded"/>
-            <span>{{messageTitle}}</span>
+
+        <!-- Мобильная версия -->
+        <div class="lg:hidden fixed bottom-0 left-0 w-full bg-[#2C2C2C] rounded-t-xl shadow-[0px_0px_20px_-13px_black] z-[5] transition-all duration-500"
+            :class="isMenuShow ? 'translate-y-0' : 'translate-y-full'">
+            <div class="flex flex-col gap-6 p-6 text-white">
+                <NuxtLink
+                    class="flex flex-col after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"
+                    to="/" @click="isMenuShow = false">
+                    Главная
+                </NuxtLink>
+                <NuxtLink
+                    class="flex flex-col after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"
+                    to="/vacancies" v-if="userStore.role === 'applicant'" @click="isMenuShow = false">
+                    Вакансии
+                </NuxtLink>
+                <NuxtLink
+                    class="flex flex-col after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"
+                    to="/resumes" v-if="userStore.role === 'employer'" @click="isMenuShow = false">
+                    Резюме
+                </NuxtLink>
+                <NuxtLink
+                    class="flex flex-col after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"
+                    to="/about" @click="isMenuShow = false">
+                    О нас
+                </NuxtLink>
+                <NuxtLink to="/auth" class="flex transition-all duration-500 hover:opacity-70"
+                    @click="isMenuShow = false">
+                    <Icon class="text-3xl text-sky-500" name="material-symbols:person" />
+                </NuxtLink>
+            </div>
+        </div>
+        <div @click="isMenuShow = false" class="lg:hidden fixed z-[4] inset-0 bg-black/30"
+            :class="{ 'max-lg:hidden': !isMenuShow }"></div>
+        <button type="button" @click="messageTitle = null"
+            class="lg:fixed lg:top-10 lg:right-4 max-lg:sticky max-lg:top-4 max-lg:mx-auto z-[11] cursor-pointer flex items-center gap-2 px-4 py-1 text-base rounded-xl w-fit font-medium font-mono border-white border bg-[#2C2C2C] shadow-[0px_0px_13px_-7px_black]"
+            :class="messageType ? 'text-white' : 'text-red-500'" v-if="messageTitle">
+            <Icon class="text-2xl" name="material-symbols:close-small-rounded" />
+            <span>{{ messageTitle }}</span>
         </button>
     </header>
 </template>
